bffd82c6d1919ef91b9d377239d6a06ac04c33b7
Reed <reedbroadhead@gmail.com>
2023-03-06 11:45:49 -0600

    renders movie cards, movie dropdown and comment section

---
+++ index.js
@@ -0,0 +1,57 @@
+document.addEventListener( "DOMContentLoaded", () => {
+    fetch('http://localhost:3000/Movies')
+    .then(response => response.json())
+    .then(data => data.forEach(movie =>  renderMovie(movie)))
+})
+
+function renderMovie(movie){
+    const movieCards = document.getElementById("movie-cards")
+    const innerDiv = document.createElement("div")
+// create div
+    const movieCard = document.createElement("div")
+// name
+    const movieName = document.createElement("h1")
+    movieName.innerText = movie.name
+// img
+    const movieImg = document.createElement("img")
+    movieImg.src = movie.image
+    movieImg.alt = `${movie.name} image`
+
+    movieCard.append(movieName, movieImg, innerDiv)
+    movieCards.append(movieCard)
+
+    //  drop down stuff
+    movieImg.addEventListener("click", ()=> {
+        innerDiv.innerHTML = ""
+        innerDiv.className = "inner-div"
+        // run time 
+        const runTime = document.createElement("p")
+        runTime.innerText = `Runtime: ${movie.runTime}`
+    //  current rating
+        const movieRating = document.createElement("p")
+        movieRating.className = "rating"
+        movieRating.innerText = `potatoes: ${movie.rating}`
+    //  likeButton increas 
+        const ratingIncreas = document.createElement("button")
+        ratingIncreas.className = "rating-increas"
+        ratingIncreas.innerText = "+"
+    //  rating decrease
+        const ratingDecrease = document.createElement("button")
+        ratingDecrease.className = "rating-decrease"
+        ratingDecrease.innerText = "-"
+        innerDiv.append(runTime, movieRating, ratingIncreas, ratingDecrease)
+    //  comment section div
+        const commentDiv = document.createElement('div')
+        commentDiv.className = "comment-section"
+        // form 
+        const form = document.createElement("form")
+        form.className = "comment-form"
+        form.innerText = "form"
+        // append
+        commentDiv.append(form)
+        movieCard.append(commentDiv)
+    
+    })
+
+}
+
